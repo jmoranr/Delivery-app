55db7f31ab4d9a6803c220d4d1e0f1ba05c793c0
Jesus Moran <74533751+jmoranr@users.noreply.github.com>
2025-03-11 19:57:22 +0100

    Update mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -8,41 +8,41 @@ db.orders.insertMany ([
     deliveryAddress: 'Av Fuenlabrada',
     totalPrice: 20,
     orderStatus: 'PENDING',
-    createdAt: '2025-03-01'
+    createdAt: new Date("2025-03-01T00:00:00.000+00:00")
   },
   {
     customerName: 'Pablo',
     deliveryAddress: 'Cl Gran Via',
     totalPrice: 15,
     orderStatus: 'ON_THE_WAY',
-    createdAt: '2025-03-05'
+    createdAt: new Date("2025-03-05T00:00:00.000+00:00")
   },
   {
     customerName: 'Blanca',
     deliveryAddress: 'Cl del Sol',
     totalPrice: 13,
     orderStatus: 'DELIVERED',
-    createdAt: '2025-03-08'
+    createdAt: new Date("2025-03-08T00:00:00.000+00:00")
   },
   {
     customerName: 'Maria',
     deliveryAddress: 'C1 Mayor',
     totalPrice: 24,
     orderStatus: 'PENDING',
-    createdAt: '2025-03-10'
+    createdAt: new Date("2025-03-10T00:00:00.000+00:00")
   },
   {
     customerName: 'Carlos',
     deliveryAddress: 'Av San Blas',
     totalPrice: 29,
     orderStatus: 'DELIVERED',
-    createdAt: '2025-03-15'
+    createdAt: new Date("2025-03-15T00:00:00.000+00:00")
   },
   {
     customerName: 'Marta',
     deliveryAddress: 'Av Delicias',
     totalPrice: 19,
     orderStatus: 'ON_THE_WAY',
-    createdAt: '2025-03-20'
-  }
+    createdAt: new Date("2025-03-20T00:00:00.000+00:00")
+  },
 ])
